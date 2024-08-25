83e1a69fafe8a6d5aa835bf4dee6784c9579dccf
maxiducoli <maxiducoli@gmail.com>
2024-08-25 14:25:46 -0300

    Agregar archivos de proyecto.

---
+++ WE2002 - MultiTool -= by CARP =- @maxiducoli/frmWEMiniTool.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace WE2002___MultiTool____by_CARP_____maxiducoli
+{
+    partial class frmWEMiniTool
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            linkLabel1 = new LinkLabel();
+            pictureBox1 = new PictureBox();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.BackgroundImageLayout = ImageLayout.Stretch;
+            button1.Location = new Point(12, 12);
+            button1.Name = "button1";
+            button1.Size = new Size(99, 43);
+            button1.TabIndex = 0;
+            button1.Text = "Creador de estadios";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.BackgroundImageLayout = ImageLayout.Stretch;
+            button2.Location = new Point(13, 110);
+            button2.Name = "button2";
+            button2.Size = new Size(98, 43);
+            button2.TabIndex = 1;
+            button2.Text = "Creador de RA";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.BackgroundImageLayout = ImageLayout.Stretch;
+            button3.Location = new Point(116, 12);
+            button3.Name = "button3";
+            button3.Size = new Size(99, 43);
+            button3.TabIndex = 2;
+            button3.Text = "Creador de BIN";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // linkLabel1
+            // 
+            linkLabel1.AutoSize = true;
+            linkLabel1.Location = new Point(60, 175);
+            linkLabel1.Name = "linkLabel1";
+            linkLabel1.Size = new Size(270, 15);
+            linkLabel1.TabIndex = 3;
+            linkLabel1.TabStop = true;
+            linkLabel1.Text = "Ayúdame con un cafecito - Help me with a coffee";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.cafecito;
+            pictureBox1.Location = new Point(13, 162);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(41, 28);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 4;
+            pictureBox1.TabStop = false;
+            // 
+            // button4
+            // 
+            button4.BackgroundImageLayout = ImageLayout.Stretch;
+            button4.Location = new Point(12, 61);
+            button4.Name = "button4";
+            button4.Size = new Size(99, 43);
+            button4.TabIndex = 5;
+            button4.Text = "Extractor de estadios";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(221, 61);
+            button5.Name = "button5";
+            button5.Size = new Size(99, 43);
+            button5.TabIndex = 6;
+            button5.Text = "La Pinta";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(221, 12);
+            button6.Name = "button6";
+            button6.Size = new Size(99, 43);
+            button6.TabIndex = 7;
+            button6.Text = "Creador de T_Name";
+            button6.UseVisualStyleBackColor = true;
+            button6.Click += button6_Click;
+            // 
+            // button7
+            // 
+            button7.Location = new Point(116, 61);
+            button7.Name = "button7";
+            button7.Size = new Size(99, 43);
+            button7.TabIndex = 8;
+            button7.Text = "Extraer e insertar BIN";
+            button7.UseVisualStyleBackColor = true;
+            button7.Click += button7_Click;
+            // 
+            // button8
+            // 
+            button8.BackgroundImageLayout = ImageLayout.Stretch;
+            button8.Location = new Point(116, 110);
+            button8.Name = "button8";
+            button8.Size = new Size(98, 43);
+            button8.TabIndex = 9;
+            button8.Text = "Extractor de VAGs";
+            button8.UseVisualStyleBackColor = true;
+            button8.Click += button8_Click;
+            // 
+            // frmWEMiniTool
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(347, 212);
+            Controls.Add(button8);
+            Controls.Add(button7);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(pictureBox1);
+            Controls.Add(linkLabel1);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            FormBorderStyle = FormBorderStyle.FixedToolWindow;
+            Name = "frmWEMiniTool";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Winning Eleven 2002 - MultiTool -= by CARP =-";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private LinkLabel linkLabel1;
+        private PictureBox pictureBox1;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+    }
+}
---
+++ WE2002 - MultiTool -= by CARP =- @maxiducoli/frmWEMiniTool.cs
@@ -0,0 +1,147 @@
+using System.Reflection;
+
+namespace WE2002___MultiTool____by_CARP_____maxiducoli
+{
+    public partial class frmWEMiniTool : Form
+    {
+        string basePath = Application.StartupPath + "dll\\";
+        public frmWEMiniTool()
+        {
+            InitializeComponent();
+        }
+        /*
+                private void CargarForm(string nombreDLL, string nameSpace)
+                {
+                    // Ruta a la DLL
+                    string dllPath = basePath + nombreDLL;
+
+                    // Cargar el ensamblado
+                    Assembly assembly = Assembly.LoadFrom(dllPath);
+
+                    // Obtener el tipo del formulario (ajusta el nombre del namespace y de la clase)
+                    Type formType = assembly.GetType(nameSpace);
+
+                    if (formType == null)
+                    {
+                        Console.WriteLine("No se pudo encontrar el tipo de formulario.");
+                        return;
+                    }
+
+                    // Crear una instancia del formulario
+                    Form form = (Form)Activator.CreateInstance(formType);
+
+                    // Verificar si la instancia es un formulario
+                    if (form != null)
+                    {
+                        // Iniciar la aplicación con el formulario
+                        Application.EnableVisualStyles();
+        //                Application.SetCompatibleTextRenderingDefault(false);
+                        form.ShowDialog();
+                    }
+                    else
+                    {
+                        Console.WriteLine("No se pudo crear una instancia del formulario.");
+                    }
+                }
+        */
+
+        private void CargarForm(string nombreDLL, string nameSpace)
+        {
+            try
+            {
+                // Ruta a la DLL
+                string dllPath = basePath + nombreDLL;
+
+                // Cargar el ensamblado
+                Assembly assembly = Assembly.LoadFrom(dllPath);
+
+                // Obtener el tipo del formulario (ajusta el nombre del namespace y de la clase)
+                Type formType = assembly.GetType(nameSpace);
+
+                if (formType == null)
+                {
+                    Console.WriteLine("No se pudo encontrar el tipo de formulario.");
+                    return;
+                }
+
+                // Crear una instancia del formulario
+                Form form = (Form)Activator.CreateInstance(formType);
+
+                // Verificar si la instancia es un formulario
+                if (form != null)
+                {
+                    // Iniciar la aplicación con el formulario
+                    Application.EnableVisualStyles();
+                    // Application.SetCompatibleTextRenderingDefault(false);
+                    form.ShowDialog();
+                }
+                else
+                {
+                    Console.WriteLine("No se pudo crear una instancia del formulario.");
+                }
+            }
+            catch (UnauthorizedAccessException ex)
+            {
+                Console.WriteLine("Error de acceso no autorizado: " + ex.Message);
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine("Archivo no encontrado: " + ex.Message);
+            }
+            catch (FileLoadException ex)
+            {
+                Console.WriteLine("Error al cargar el archivo: " + ex.Message);
+            }
+            catch (BadImageFormatException ex)
+            {
+                Console.WriteLine("El archivo no es un ensamblado válido: " + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error general: " + ex.Message);
+            }
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            CargarForm("Winning Eleven Stadium Maker.dll", "frmTMDMaker.frmTMDMaker");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            CargarForm("SinSalaBIN.dll", "SinSalaBIN.frmSinSalaBIN");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            CargarForm("RaMaker.dll", "RaMaker.frmRaMaker");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            CargarForm("Winnin Eleven TMD Extractor.dll", "WE_TMDMaker.frmExtractTMD");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            CargarForm("La pinta.dll", "GraphicsTools.Form1");
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            CargarForm("T_NAME Maker.dll", "T_NAME_Maker.frmT_NAME");
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            CargarForm("WE Bin File Tool.dll", "Bin_File_Tool.frmBinFileTool");
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            CargarForm("VagExtractor.dll", "VagExtractor.frmVagExtractor");
+        }
+    }
+}
